f8b2df946ef48894219d57933acaab2cc04a1048
vanloc <tovanloc19@gmail.com>
2025-05-06 20:02:30 +0700

    configure images/domains

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ images: {
+    domains: [
+      'platform-lookaside.fbsbx.com',
+     'thumbs.dreamstime.com',
+      'www.shutterstock.com',
+      'images.unsplash.com',
+      'i.pinimg.com',
+      'cdn.pixabay.com',
+      'images.pexels.com',
+      'www.gstatic.com',
+      'lh3.googleusercontent.com',
+      'firebasestorage.googleapis.com',
+      'cdn.discordapp.com',
+      'avatars.githubusercontent.com',
+      'www.gravatar.com',
+    ],
+  },
+  sassOptions: {
+      includePaths: [path.join(__dirname, "src/styles")],
+    prependData: `@use "variables" as *; @use "mixins" as *; @use "functions" as *;`,
+    outputStyle: 'compressed',
+  },
 };
 
 export default nextConfig;
