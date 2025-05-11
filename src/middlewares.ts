f7bd2517fc6595406e8baa22db3f8ccf41129e59
vanloc19 <tovanloc19@gmail.com>
2025-05-11 07:07:44 +0700

    update

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -2,33 +2,15 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const { pathname, searchParams, origin } = request.nextUrl;
+  const { pathname } = request.nextUrl;
 
   const publicPaths = ["/accounts", "/accounts/login", "/accounts/register"];
-
-  // Nếu là public path
   if (publicPaths.includes(pathname)) {
-    const token = searchParams.get("token");
-    console.log("Token from URL:", token);
-
-    if (token) {
-      // Lưu token vào cookie
-      const response = NextResponse.redirect(new URL("/", origin)); // Chuyển hướng về `/`
-      response.cookies.set("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "strict",
-        path: "/",
-        maxAge: 60 * 60 * 24, // 1 day
-      });
-      return response;
-    }
-
     return NextResponse.next();
   }
 
-  // Kiểm tra token trong cookie với các route protected
-  const token = request.cookies.get("token")?.value;
+  // Đọc cookie "token" hoặc tên khác bạn dùng trong backend
+  const token = request.cookies.get("token"); // ví dụ token
 
   if (!token) {
     return NextResponse.redirect(new URL("/accounts", request.url));
