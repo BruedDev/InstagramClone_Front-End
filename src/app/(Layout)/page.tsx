5a06d68bf1e4e1f0f6beb40c48d302a644bcf36d
vanloc19 <tovanloc19@gmail.com>
2025-05-12 05:03:46 +0700

    update

--- src/app/(Layout)/page.tsx
+++ src/app/(Layout)/page.tsx
@@ -9,20 +9,8 @@ export default function Home() {
   const handleLogout = async () => {
     try {
       await logout();
-
-      // Clear all cookies
-      document.cookie.split(";").forEach((cookie) => {
-        const cookieName = cookie.split("=")[0];
-        // Set each cookie's expiration date to the past to remove it
-        document.cookie = `${cookieName}=; Max-Age=-99999999; path=/;`;
-        document.cookie = `${cookieName}=; Max-Age=-99999999; path=/; domain=${window.location.hostname};`; // Ensures cookies from subdomains are cleared
-      });
-
-      // Clear localStorage
-      localStorage.clear();
-
-      // Redirect to accounts page
       router.push("/accounts");
+      localStorage.clear();
     } catch (error) {
       console.error("Logout failed:", error);
     }
